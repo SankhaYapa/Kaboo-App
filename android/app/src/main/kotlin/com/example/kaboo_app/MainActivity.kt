ffc4385a13d156e9d9476effa92022cb3d3eb421
SankhaYapa <sankhapriyamantha55@gmail.com>
2022-08-07 12:07:16 +0530

    update

---
+++ android/app/src/main/kotlin/com/example/kaboo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kaboo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
