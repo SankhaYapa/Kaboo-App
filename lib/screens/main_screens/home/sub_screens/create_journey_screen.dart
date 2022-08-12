86aa8921068ca7a457400dfc453d044fdbfc4f88
SankhaYapa <sankhapriyamantha55@gmail.com>
2022-08-12 17:31:20 +0530

    update

--- lib/screens/main_screens/home/sub_screens/create_journey_screen.dart
+++ lib/screens/main_screens/home/sub_screens/create_journey_screen.dart
@@ -45,6 +45,8 @@ class _CreateJourneyScreenState extends State<CreateJourneyScreen> {
                 CustomCards(
                   name: 'Get Your SIN',
                   gif: 'pin.gif',
+                  color1: Colors.white,
+                  color2: Colors.blue,
                 ),
                 SizedBox(
                   width: 10,
@@ -52,34 +54,42 @@ class _CreateJourneyScreenState extends State<CreateJourneyScreen> {
                 CustomCards(
                   name: 'Bank Account',
                   gif: 'money.gif',
+                  color1: Colors.white,
+                  color2: Color.fromARGB(255, 21, 52, 255),
                 ),
                 SizedBox(
                   width: 10,
                 ),
                 CustomCards(
-                  name: 'Local Cell Plan',
-                  gif: 'cell.gif',
-                ),
+                    name: 'Local Cell Plan',
+                    gif: 'cell.gif',
+                    color1: Colors.white,
+                    color2: Color.fromARGB(255, 90, 230, 248)),
                 SizedBox(
                   width: 10,
                 ),
                 CustomCards(
                   name: 'Government Health\n Insurance Card',
                   gif: 'trust.gif',
+                  color1: Colors.white,
+                  color2: Color.fromARGB(255, 250, 89, 177),
                 ),
                 SizedBox(
                   width: 10,
                 ),
                 CustomCards(
-                  name: 'Get your PR Card',
-                  gif: 'debit.gif',
-                ),
+                    name: 'Get your PR Card',
+                    gif: 'debit.gif',
+                    color1: Colors.white,
+                    color2: Colors.blue),
                 SizedBox(
                   width: 10,
                 ),
                 CustomCards(
                   name: 'Driver License or\n Transport card',
                   gif: 'license.gif',
+                  color1: Colors.white,
+                  color2: Color.fromARGB(255, 211, 46, 248),
                 ),
                 SizedBox(
                   width: 10,
@@ -87,6 +97,8 @@ class _CreateJourneyScreenState extends State<CreateJourneyScreen> {
                 CustomCards(
                   name: 'Map of the City',
                   gif: 'map.gif',
+                  color1: Colors.white,
+                  color2: Color.fromARGB(255, 245, 42, 218),
                 ),
                 SizedBox(
                   width: 10,
@@ -94,6 +106,8 @@ class _CreateJourneyScreenState extends State<CreateJourneyScreen> {
                 CustomCards(
                   name: 'Search for\n Accommodation',
                   gif: 'home.gif',
+                  color1: Colors.white,
+                  color2: Color.fromARGB(255, 78, 122, 245),
                 ),
                 SizedBox(
                   width: 10,
@@ -101,6 +115,8 @@ class _CreateJourneyScreenState extends State<CreateJourneyScreen> {
                 CustomCards(
                   name: 'Community Groups',
                   gif: 'community.gif',
+                  color1: Colors.white,
+                  color2: Color.fromARGB(255, 244, 252, 33),
                 ),
                 SizedBox(
                   width: 10,
@@ -108,6 +124,8 @@ class _CreateJourneyScreenState extends State<CreateJourneyScreen> {
                 CustomCards(
                   name: 'Find a Bucket List ',
                   gif: 'checklist.gif',
+                  color1: Colors.white,
+                  color2: Color.fromARGB(255, 4, 227, 67),
                 ),
                 SizedBox(
                   width: 10,
@@ -115,6 +133,8 @@ class _CreateJourneyScreenState extends State<CreateJourneyScreen> {
                 CustomCards(
                   name: 'Know 911 for\n Emergency',
                   gif: 'ring.gif',
+                  color1: Colors.white,
+                  color2: Color.fromARGB(255, 26, 240, 172),
                 ),
                 SizedBox(
                   width: 10,
@@ -129,15 +149,52 @@ class _CreateJourneyScreenState extends State<CreateJourneyScreen> {
 }
 
 class CustomCards extends StatelessWidget {
-  const CustomCards({Key? key, required this.name, required this.gif})
-      : super(key: key);
+  const CustomCards({
+    Key? key,
+    required this.name,
+    required this.gif,
+    required this.color1,
+    required this.color2,
+  }) : super(key: key);
   final String name;
   final String gif;
+  final Color color1;
+  final Color color2;
   @override
   Widget build(BuildContext context) {
     return Stack(alignment: Alignment.center, children: [
-      CustomImage(name: 'rect.png'),
+      Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40.0),
+        ),
+        elevation: 6,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          height: 231,
+          width: 287,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(40),
+            gradient: LinearGradient(
+              colors: [
+                color1,
+                color2,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          // child: Align(
+          //     alignment: Alignment.centerLeft,
+          //     child: CustomText(
+          //       text: name,
+          //       fontSize: 15,
+          //       fontWeight: FontWeight.w400,
+          //     )),
+        ),
+      ),
+      //CustomImage(name: 'rect.png'),
       Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Container(height: 95, width: 95, child: CustomIcon(iconName: gif)),
           CustomText(
